74f2e24c9432a0e2b9dc6f42d114e7fde103619a
JD <jharalson29@gmail.com>
2021-12-02 13:06:59 -0500

    Updating my code. Still need to get past 11 in the loop.

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
-var saveBtn = ".saveBtn"
-var today = moment();
-$("#currentDay").text(today.format("LLLL"));
+$(document).ready(function () {
+    var saveBtn = ".saveBtn";
+    var today = moment();
+    $("#currentDay").text(today.format("MMMM Do YYYY"));
 
-function save() {
-    
+    $(".saveBtn").on("click", function () {
+        var textValue = $(this).siblings(".description").val();
+        var timeKey = $(this).parent().attr("id");
+            localStorage.setItem(timeKey, textValue);
+})
+})
+
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+$("#18 .description").val(localStorage.getItem("18"));
+$("#19 .description").val(localStorage.getItem("19"));
+$("#20 .description").val(localStorage.getItem("20"));
+$("#21 .description").val(localStorage.getItem("21"));
+
+
+
+function cssUpdate () {
+   var currentHour = moment().hour();
+   $(".time-block").each(function () {
+       var divTimeId = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(divTimeId, currentHour);
+       if (divTimeId < currentHour) {
+           $(this).removeClass("present");
+           $(this).removeClass("future");
+           $(this).addClass("past");
+       } else if (divTimeId === currentHour) {
+           $(this).removeClass("past");
+           $(this).removeClass("future");
+           $(this).addClass("present");
+       } else if (divTimeId > currentHour) {
+           $(this).removeClass("past");
+           $(this).removeClass("present");
+           $(this).addClass("future");
+       }
+   })
 }
 
-saveBtn.addEventListener("click", save)
+cssUpdate ();
+
